7421b6b83bd9975e755dd0c40388f865367f85bb
Eduardo Asafe <e@asafe.dev>
2024-06-17 14:15:54 -0300

    Update comments

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod types;
 mod database;
 
 //until we gather enough data to actually try and predict chat matches, the suggestions work as follows:
-// users are scored (once per day) according to several metrics (see database::create_grades) and divided into bins
+// users are scored (once per day) according to some metrics (see database::create_grades)
 // these scores are sorted, paginated, then sorted again by online status and random() before being handed to clients
 fn main() {
     println!("Rebuilding...");
