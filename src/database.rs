15f17bba6c67bc2a701f3b8e80555ea1b4b72fba
Eduardo Asafe <e@asafe.dev>
2024-12-27 15:08:45 -0300

    more bins

--- src/database.rs
+++ src/database.rs
@@ -8,7 +8,7 @@ pub fn connect() -> Result<Client, Error> {
 pub fn create_grades(client: &mut Client) -> Result<Vec<Grade>, Error> {
     //the "score" of a user is just the sum of a few metrics
     // karma is (linearly) converted to the range 1..255 so it doesn't obscure the other metrics
-    // users are then put into bins top 2%, top 20%, top 50%, rest
+    // users are then put into bins top 2%, top 10%, top 20%, top 35%, top 55%, rest
     let query = "
         select  id,
                 cast((raw_score + raw_score * backer_bonus) as integer) score
@@ -21,7 +21,6 @@ pub fn create_grades(client: &mut Client) -> Result<Vec<Grade>, Error> {
                                     (case when u.backer then 0.2 else 0 end) backer_bonus,
                                     ((select count(1) from complete_profiles where completer = u.id) * 2) fullness
                             from users u
-                            where not temporary
                             order by karma desc)
                 select id,
                        ((greatest(karma, 0) * 255 / (select karma from k limit 1)) +
@@ -50,8 +49,6 @@ pub fn create_suggestions(grades: &mut [Grade]) -> String {
         "insert into suggestions (suggested, score, bin) values".to_owned();
     let total = grades.len();
 
-    grades.sort_unstable_by(|s, s2| s2.score.cmp(&s.score));
-
     for (i, gr) in grades.iter().enumerate() {
         let token = if i == total - 1 { ";" } else { "," };
         let percent = (i as f64 / total as f64) * 100.0;
@@ -59,12 +56,16 @@ pub fn create_suggestions(grades: &mut [Grade]) -> String {
 
         if percent <= 2.0 {
             bin = 1;
-        } else if percent <= 20.0 {
+        } else if percent <= 10.0 {
             bin = 2;
-        } else if percent <= 50.0 {
+        } else if percent <= 20.0 {
             bin = 3;
-        } else {
+        } else if percent <= 35.0 {
             bin = 4;
+        } else if percent <= 55.0 {
+            bin = 5;
+        } else {
+            bin = 6;
         }
 
         query.push_str(&format!(
